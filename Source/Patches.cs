75cad01139aea7ff3abb7d7e240a58f92c3fa52f
Tobias Berger <tobi.berger13@gmail.com>
2020-09-19 16:43:50 +0200

    Initial commit

---
+++ Source/Patches.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using Verse;
+
+namespace NoNutrientIngredients
+{
+    [StaticConstructorOnStartup]
+    public static class NoNutrientIngredientsMain
+    {
+        static NoNutrientIngredientsMain()
+        {
+            new HarmonyLib.Harmony("tobs.nonutrientingredients.mod").PatchAll();
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(Building_NutrientPasteDispenser), nameof(Building_NutrientPasteDispenser.TryDispenseFood))]
+        public static class NutrientPatch
+        {
+            static void Postfix(ref Thing __result)
+            {
+                __result.TryGetComp<CompIngredients>()?.ingredients.Clear();
+            }
+        }
+    }
+}
